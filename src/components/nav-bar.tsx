7966bf96e501551fb3f410fa648e1f67f3b1c468
ChibuzoEkwue <chieezy5@gmail.com>
2025-09-09 17:45:08 +0100

    initial commit

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header className="flex h-20 w-full items-center justify-center border-[1px] border-b-border">
+      <nav className="container">
+        <h2 className="text-xl">CI/CD Mini Project</h2>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
